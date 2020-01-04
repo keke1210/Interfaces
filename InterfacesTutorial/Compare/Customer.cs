b92c70a1d712291a2fbd2944b2fe835de248ce94
skerdi berberi <skerdi19berberi@gmail.com>
2020-01-04 19:56:47 +0100

    IComparable<T> & IComparer<T> Interfaces

--- InterfacesTutorial/Compare/Customer.cs
+++ InterfacesTutorial/Compare/Customer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
-namespace InterfacesTutorial
+namespace InterfacesTutorial.Compare
 {
     public class Customer : IComparable<Customer>
     {
